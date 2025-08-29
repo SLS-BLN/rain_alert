20d627bfe4cc64c6a0c264fa8df4e22cb4039330
Stefan Schlicker <stefanschlicker@posteo.de>
2025-08-29 08:53:25 +0200

    Add a check_for_rain function

--- main.py
+++ main.py
@@ -8,29 +8,42 @@ def load_config(path=".env"):
     return dotenv_values(path)
 
 
-def build_forecast_url(endpoint, lat, lon, key):
-    return f"{endpoint}?lat={lat}&lon={lon}&appid={key}"
-
-
-def fetch_forecast(url):
+def fetch_forecast(url, params):
     try:
-        response = requests.get(url, timeout=10)
+        response = requests.get(url, params, timeout=10)
         response.raise_for_status()
         return response.json()
     except RequestException as e:
         return {"error": str(e)}
 
 
+def check_for_rain(forecast):
+    for item in forecast:
+        if item["weather"][0]["id"] < 700:
+            return True
+    return False
+
+
 def main():
     config = load_config()
-    url = build_forecast_url(
-        config["API_ENDPOINT"],
-        config["LATITUDE"],
-        config["LONGITUDE"],
-        config["API_KEY"]
-    )
-    forecast = fetch_forecast(url)
-    print(forecast)
+
+    url = config["API_ENDPOINT"]
+    weather_params = {
+        "lat": config["LATITUDE"],
+        "lon": config["LONGITUDE"],
+        "appid": config["API_KEY"],
+        # To limit the number of timestamps in the API response, set up cnt.
+        # 3-hour steps - 4 timestamps = 12 hours
+        "cnt": 4
+    }
+
+    forecast = fetch_forecast(url, weather_params)["list"]
+    will_rain_today = check_for_rain(forecast)
+
+    if will_rain_today:
+        print("It will rain today!")
+    else:
+        print("It will not rain today.")
 
 
 if __name__ == "__main__":
